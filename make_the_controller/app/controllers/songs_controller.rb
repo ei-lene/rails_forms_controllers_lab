6a730df14dc6cdf0ad07b09745f24bdb50137945
ei-lene <eileneheng@gmail.com>
2013-03-13 17:09:25 -0400

    add artists to songs
    f.selected --> very abstracted
    select html form --> not abstracted

--- make_the_controller/app/controllers/songs_controller.rb
+++ make_the_controller/app/controllers/songs_controller.rb
@@ -8,7 +8,23 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new(params[:song])
+    ## Abstract method with f.select
+    # @song = Song.new(params[:song]) 
+    
+    ## This does with the longer html and select code
+      # <select name="song[artist]">
+      #   <% Artist.all.each do |a| %>
+      #     <option name="<%= a.id %>">
+      #       <%= a.name %>
+      #     </option>
+      #   <% end %>
+      # </select>
+
+    @song = Song.new(:name => params[:song][:name])
+    @song.build_artist(:name => params[:song][:artist])
+
+    @song.save
+
     if @song.save
       redirect_to @song, notice: 'Song was successfully created.'
     else
--- make_the_controller/db/schema.rb
+++ make_the_controller/db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(:version => 20130312160321) do
 
   create_table "artists", :force => true do |t|
     t.string   "name"
+    t.string   "birth_year"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
